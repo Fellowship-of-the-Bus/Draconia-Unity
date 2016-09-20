527c5b65784fffbac577eac03b5d4cb4a9ce93bd
Abraham Dubrisingh <A.Dubrisingh@gmail.com>
2016-09-20 19:08:07 -0400

    Rotation test

--- Assets/Script/CameraController.cs
+++ Assets/Script/CameraController.cs
@@ -4,6 +4,7 @@ using System.Collections;
 public class CameraController : MonoBehaviour {
 	public float mouseSensitivity = 0.01f;
 	private Vector3 lastPosition;
+	public float smooth = 2.0F;
 
 	// Use this for initialization
 	void Start () {
@@ -23,5 +24,13 @@ public class CameraController : MonoBehaviour {
 			transform.Translate(delta.x * -mouseSensitivity, delta.y * -mouseSensitivity, 0f);
 			lastPosition = Input.mousePosition;
 		}
+	}
+
+	public void rotateLeft () {
+		transform.Rotate(Vector3.forward * 90, Space.World);
+	}
+
+	public void rotateRight () {
+		transform.Rotate(Vector3.back * 90, Space.World);
 	}
 }
