5f9d36fc491fd28fdd519978882844f8f4b855f4
Travis Bartlett <trbartle@uwaterloo.ca>
2016-09-24 16:49:47 -0400

    Pawns are on top of board

--- Assets/Script/PlayerControl.cs
+++ Assets/Script/PlayerControl.cs
@@ -6,20 +6,20 @@ public class PlayerControl : MonoBehaviour {
   private GameManager gameManager;  // GameObject responsible for the management of the game
 
   public int moveRange = 4;
-  
+
   // Use this for initialization
   void Start () {
     PlayerCam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     gameManager = gameObject.GetComponent<GameManager>();
   }
-  
+
   // Update is called once per frame
   void Update () {
     GetMouseInputs();
   }
-  
+
   // Detect Mouse Inputs
-  void GetMouseInputs() { 
+  void GetMouseInputs() {
     GameObject clickedObject = gameManager.getClicked(PlayerCam);
 
     // Select a piece
@@ -31,9 +31,9 @@ public class PlayerControl : MonoBehaviour {
 
       if (gameManager.SelectedPiece) {
         Vector3 selectedCoord;
-        
+
         if(clickedObject.tag == ("Cube")) {
-          selectedCoord = new Vector3(clickedObject.transform.position.x, clickedObject.transform.position.y, clickedObject.transform.position.z);
+          selectedCoord = new Vector3(clickedObject.transform.position.x, clickedObject.transform.position.y+1, clickedObject.transform.position.z);
           gameManager.MovePiece(selectedCoord);
         }
       }
