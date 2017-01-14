7d545efef644a8d8b3af3fb780276e69575446f5
Abraham Dubrisingh <A.Dubrisingh@gmail.com>
2017-01-14 17:47:42 -0500

    Added ranged targeting skill subclass

--- Assets/Script/Skills/Active/Ranged.cs
+++ Assets/Script/Skills/Active/Ranged.cs
@@ -1,31 +1,11 @@
 using UnityEngine;
 using System.Collections.Generic;
 
-public class Ranged: ActiveSkill {
+public class Ranged: RangedTargeting {
   public Ranged() {
     useLos = false;
     name = "Ranged";
   }
-  public override List<GameObject> getTargets() {
-    range = self.attr.weaponRange;
-
-    GameManager gm = GameManager.get;
-    List<Tile> tiles = gm.getTilesWithinRange(self.curTile, range);
-    List<GameObject> targets = new List<GameObject>();
-    Vector3 source = new Vector3(gm.SelectedPiece.transform.position.x, gm.SelectedPiece.transform.position.y + 0.25f, gm.SelectedPiece.transform.position.z);
-    foreach (Tile t in tiles) {
-      if (t.occupied()) {
-        GameObject o = t.occupant;
-        Vector3 target = new Vector3(o.transform.position.x, o.transform.position.y + 0.25f, o.transform.position.z);
-        RaycastHit hitInfo;
-        if (gm.checkLine(source, target, out hitInfo)) {
-           targets.Add(o);
-        }
-      }
-    }
-    return targets;
-  }
-
 
   public override int calculateDamage(Character source, Character target) {
     return (int)(source.attr.strength*(1+level*0.1) - target.attr.physicalDefense);
