5f2eacac8ed4381b987eef0a9ba9811f61da0a93
Rob Schluntz <saitou1024@gmail.com>
2017-01-21 16:10:08 -0500

    maintain lists of characters, change text to match damage amount

--- Assets/Script/Objective/Rout.cs
+++ Assets/Script/Objective/Rout.cs
@@ -6,8 +6,8 @@ using System.Collections;
 
 public class Rout : Objective {
   override public bool isMet(GameManager game) {
-    GameObject[] enemies = GameObject.FindGameObjectsWithTag("PiecePlayer2");
-    if (enemies.Length == 0) return true;
+    List<GameObject> enemies = game.enemies;
+    if (enemies.Count == 0) return true;
     else return false;
   }
 }
