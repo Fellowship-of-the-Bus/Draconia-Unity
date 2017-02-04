de57ab3ddf46749e165f069f2d880ddd0296fd52
Abraham Dubrisingh <A.Dubrisingh@gmail.com>
2017-02-04 17:15:48 -0500

    Added Exsanguination aura

--- Assets/Script/Skills/Passive/Warlock/Exsanguination Aura.cs
+++ Assets/Script/Skills/Passive/Warlock/Exsanguination Aura.cs
@@ -1,10 +1,9 @@
-using UnityEngine;
+﻿using UnityEngine;
 using System;
 using System.Collections.Generic;
 
-public class SlowAura: PassiveSkill {
+public class ExsanguinationAura: PassiveSkill {
   public int aoe {get; set;}
-
 
   public override List<GameObject> getTargets() {
     List<GameObject> targets = new List<GameObject>();
@@ -14,23 +13,24 @@ public class SlowAura: PassiveSkill {
 
   protected override void onApply(Character c) {
     range = 0;
-    aoe = 2;
+    aoe = 3;
     useLos = false;
-    name = "SlowAura";
+    name = "ExsanguinationAura";
 
-    Func<SlowEffect> f = () => {
-      SlowEffect eff = new SlowEffect();
+    Func<BleedEffect> f = () => {
+      BleedEffect eff = new BleedEffect();
+      eff.level = level;
       eff.duration = -1;
+      eff.damage = owner.attr.intelligence;
       return eff;
     };
 
-    Aura<SlowEffect> e = new Aura<SlowEffect>(aoe, f, true);
+    Aura<BleedEffect> e = new Aura<BleedEffect>(aoe, f, true);
     e.level = level;
     e.duration = -1;
     e.applyToSelf = true;
 
     owner.applyEffect(e);
-
   }
 
   protected override void onActivate() {
