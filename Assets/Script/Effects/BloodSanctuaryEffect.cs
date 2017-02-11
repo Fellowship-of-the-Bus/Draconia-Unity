897d479e61db21572fef21185b16fe187638538e
Travis Bartlett <trbartle@uwaterloo.ca>
2017-02-11 18:48:34 -0500

    Actually add blood sanctuary skill this time

---
+++ Assets/Script/Effects/BloodSanctuaryEffect.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BloodSanctuaryEffect : DurationEffect {
+  public Character caster;
+  List<Character> effected = new List<Character>();
+  protected override void onActivate() {
+    attachListener(GameManager.get.eventManager, EventHook.endTurn);
+    attachListener(GameManager.get.eventManager, EventHook.enterTile);
+    attachListener(caster, EventHook.endTurn);
+  }
+
+  protected override void onDeactivateListeners() {
+    detachListener(GameManager.get.eventManager);
+    detachListener(caster);
+  }
+
+  float healing(Character c) {
+    return caster.intelligence * 1f * c.healingMultiplier;
+  }
+
+  protected override void additionalEffect(Event e) {
+    if (e.sender == null && e.hook == EventHook.endTurn) {
+      Character occupant = null;
+      if (ownerTile.occupant != null) {
+        occupant = ownerTile.occupant.GetComponent<Character>();
+      }
+      if (occupant != null && occupant == e.endTurnChar && !(effected.Contains(occupant))) {
+        occupant.takeHealing((int)(healing(occupant)));
+      }
+      effected.Clear();
+    } else if (e.hook == EventHook.enterTile && e.position == ownerTile.transform.position) {
+      effected.Add(e.sender);
+      e.sender.takeHealing((int)(healing(e.sender)));
+    }
+  }
+
+  public override bool shouldDecrement(Event e) {
+    return e.sender == caster;
+  }
+}
---
+++ Assets/Script/Skills/Active/BloodPriest/BloodSanctuary.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class BloodSanctuary: ActiveSkill, AoeSkill {
+  public int aoe {get; set;}
+  public bool effectsTiles {get; set;}
+
+  public BloodSanctuary() {
+    range = 3;
+    aoe = 0;
+    useLos = false;
+    name = "Blood Sanctuary";
+    effectsTiles = true;
+    maxCooldown = 1;
+  }
+
+  public override List<GameObject> getTargets() {
+    GameManager gm = GameManager.get;
+    List<Tile> tiles = gm.getTilesWithinRange(self.curTile, range);
+    List<GameObject> targets = new List<GameObject>();
+    foreach (Tile t in tiles) {
+      targets.Add(t.gameObject);
+    }
+    targets.Add(self.curTile.gameObject);
+    return targets;
+  }
+
+  public List<GameObject> getTargetsInAoe(Vector3 position) {
+    GameManager gm = GameManager.get;
+    List<Tile> tiles = gm.getTilesWithinRange(gm.getTile(position), aoe);
+    List<GameObject> targets = new List<GameObject>();
+    foreach (Tile t in tiles) {
+        targets.Add(t.gameObject);
+    }
+    targets.Add(gm.getTile(position).gameObject);
+    return targets;
+  }
+
+  public override void tileEffects(Tile target) {
+    self.takeDamage((int)(self.intelligence * 0.5));
+    BloodSanctuaryEffect block = new BloodSanctuaryEffect();
+    block.level = level;
+    block.duration = 2;
+    block.caster = self;
+    target.applyEffect(block);
+  }
+
+  public override int calculateDamage(Character source, Character target) {
+    return 0;
+  }
+
+
+}
