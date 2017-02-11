21890944a554b62428361b9574c5ba7c8c9ebd4c
Abraham Dubrisingh <A.Dubrisingh@gmail.com>
2017-02-11 15:43:29 -0500

    Added shuriken skill

---
+++ Assets/Script/Skills/Active/Ninja/Shuriken.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Shuriken: SingleTarget {
+  public Shuriken() {
+    range = 3;
+    useLos = true;
+    name = "Shuriken";
+    maxCooldown = 2;
+  }
+
+  public override int calculateDamage(Character source, Character target) {
+    return (int)(source.strength*(1+level*0.1) - target.physicalDefense);
+  }
+}
