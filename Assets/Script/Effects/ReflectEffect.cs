6ad88b83e7f6c970566bc84ac038c0d48367e63e
Travis Bartlett <trbartle@uwaterloo.ca>
2017-03-18 16:24:55 -0400

    Add reflect skill

---
+++ Assets/Script/Effects/ReflectEffect.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ReflectEffect : DurationEffect {
+  protected override void onActivate() {
+    attachListener(owner, EventHook.preDamage);
+  }
+  protected override void onDeactivate() {
+    detachListener(owner);
+  }
+
+  protected override void additionalEffect(Event e) {
+    if (e.sender is Character) {
+      e.newTarget = e.sender;
+      owner.removeEffect(this);
+    }
+  }
+}
---
+++ Assets/Script/Skills/Active/BloodPriest/Reflect.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class Reflect: SingleTarget {
+  public Reflect() {
+    range = 5;
+    useLos = false;
+    name = "Reflect";
+    maxCooldown = 2;
+  }
+
+  public override void additionalEffects (Character target) {
+    int cost = self.maxHealth / 10;
+    if (self.curHealth > cost) {
+      self.takeDamage(cost);
+      ReflectEffect e = new ReflectEffect();
+      e.level = level;
+      e.duration = 2;
+      target.applyEffect(e);
+
+
+      Event buffEvent = new Event(self, EventHook.useBuffSkill);
+      buffEvent.appliedBuff = e;
+      self.onEvent(buffEvent);
+    }
+  }
+
+}
