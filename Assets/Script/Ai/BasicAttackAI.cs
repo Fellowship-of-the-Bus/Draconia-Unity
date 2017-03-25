9c0bcc24a86f7e6fcb76aa1773a9cc018b9a5cb3
Abraham Dubrisingh <A.Dubrisingh@gmail.com>
2017-03-25 14:27:04 -0400

    Improved AI, assigned Int

--- Assets/Script/Ai/BasicAttackAI.cs
+++ Assets/Script/Ai/BasicAttackAI.cs
@@ -11,6 +11,7 @@ public class BasicAttackAI : BaseAttackAI {
     List<GameObject> targets = skill.getTargets();
     if (targets.Count == 0) return;
     List<Character> c = new List<Character>(targets.Take(1).Select(x => x.GetComponent<Character>()));
+    c = new List<Character>(c.Filter((character) => character.team != owner.team));
     List<Effected> e = new List<Effected>(c.Map(x => x as Effected));
     owner.attackWithSkill(skill, e);
   }
