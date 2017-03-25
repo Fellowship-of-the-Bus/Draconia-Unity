7bcdb15dab3bd0c4adaabc988562aae78d0080a2
Abraham Dubrisingh <A.Dubrisingh@gmail.com>
2017-03-25 14:11:46 -0400

    Added basic menu

---
+++ Assets/Script/MainMenu.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu: MonoBehaviour {
+  public MainMenu() {}
+
+  public void test() {
+    SceneManager.LoadSceneAsync ("testMap");
+  }
+ }
