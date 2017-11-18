912ab96d75f8fe71efb6a966901f7835d50d1f1c
Kevin Wu <kevin.kaiyu.wu@gmail.com>
2017-11-18 13:26:33 -0500

    add grid thickness field in options

--- Assets/Script/Options.cs
+++ Assets/Script/Options.cs
@@ -27,13 +27,13 @@ public class Options {
     }
   }
   [System.Serializable]
-  public class OpSlideField : OpField {
+  public class OpSliderField : OpField {
     public int val;
     public int min;
     public int max;
     public Text slideVal;
     public Slider slider;
-    public OpSlideField(int val, int min, int max, string name) : base(name) {
+    public OpSliderField(int val, int min, int max, string name) : base(name) {
       this.val = val;
       this.min = min;
       this.max = max;
@@ -62,11 +62,7 @@ public class Options {
   public class OptionType {
     readonly int type;
     public readonly static OptionType disAni = new OptionType(0);
-    public readonly static OptionType test1 = new OptionType(1);
-    public readonly static OptionType test2 = new OptionType(2);
-    public readonly static OptionType test3 = new OptionType(3);
-    public readonly static OptionType test4 = new OptionType(4);
-    public readonly static OptionType test5 = new OptionType(5);
+    public readonly static OptionType gridThickness = new OptionType(1);
 
     public OptionType(int i) {
       type = i;
@@ -81,14 +77,13 @@ public class Options {
 
   public static OpField[] displayedOptions = new OpField[] {
                                                     new OpToggleField(true, "Display Animations"),
-                                                    new OpToggleField(true, "Display Animations1"),
-                                                    new OpToggleField(true, "Display Animations2"),
-                                                    new OpSlideField(10, 5,15,"Display Animations3"),
-                                                    new OpSlideField(5,5,15, "Display Animations4"),
-                                                    new OpSlideField(60,30,75, "Display Animations5")};
+                                                    new OpSliderField(0,0,4, "Grid Thickneess")};
   //Visible fields
   public static bool displayAnimation {
     get { return ((OpToggleField)displayedOptions[OptionType.disAni]).val;}
+  }
+  public static int gridThickness {
+    get { return ((OpSliderField)displayedOptions[OptionType.gridThickness]).val;}
   }
 
   //internal use fields
