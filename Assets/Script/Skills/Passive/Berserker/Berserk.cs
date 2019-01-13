947aaa822ca6f3b90008daf6f7782da180391603
Rob Schluntz <rschlunt@uwaterloo.ca>
2019-01-13 16:20:11 -0500

    Cleanup in Character and BattleCharacter to reduce clutter in the Inspector

--- Assets/Script/Skills/Passive/Berserker/Berserk.cs
+++ Assets/Script/Skills/Passive/Berserker/Berserk.cs
@@ -16,7 +16,7 @@ public class Berserk : PassiveSkill {
       return;
     }
     percentage = percentage * (1 + (level -1));
-    owner.curAction = (float)Math.Min(owner.curAction + owner.maxAction*percentage, owner.maxAction);
+    owner.curAction = (float)Math.Min(owner.curAction + BattleCharacter.maxAction*percentage, BattleCharacter.maxAction);
     ActionQueue.get.updateTime(owner.gameObject);
   }
 }
