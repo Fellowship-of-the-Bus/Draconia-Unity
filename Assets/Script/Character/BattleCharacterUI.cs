5a0a3e8906fe13a23c849da719da82b4b4189c7a
Rob Schluntz <rschlunt@uwaterloo.ca>
2019-03-09 18:13:44 -0500

    Silence warning for BattleCharacterUI name

--- Assets/Script/Character/BattleCharacterUI.cs
+++ Assets/Script/Character/BattleCharacterUI.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 
 public class BattleCharacterUI : MonoBehaviour {
-  public CustomText name;
+  public new CustomText name;
   public HealthBarManager healthBars;
 
   void Update() {
