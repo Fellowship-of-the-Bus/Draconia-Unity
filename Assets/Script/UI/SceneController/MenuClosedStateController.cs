d2b757f8da746692c47be53f429d83d9fa167829
Rob Schluntz <rschlunt@uwaterloo.ca>
2019-03-17 11:58:52 -0400

    Modify menu transition to fade in/out

---
+++ Assets/Script/UI/SceneController/MenuClosedStateController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Attached to the Closed state on The Menu Animator, to ensure that closing animation fully
+// plays before the menu is set inactive
+
+public class MenuClosedStateController : StateMachineBehaviour {
+  override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    animator.gameObject.SetActive(false);
+  }
+
+  override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex) {
+    animator.gameObject.SetActive(true);
+  }
+}
