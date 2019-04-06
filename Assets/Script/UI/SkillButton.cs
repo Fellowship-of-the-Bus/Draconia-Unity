b35f7298b5ad3b2b3f71d9a605bb5fc117a69255
Rob Schluntz <rschlunt@uwaterloo.ca>
2019-04-06 19:08:49 -0400

    Fix skill button stretching and refactor SkillButton component accesses; fixes #59

---
+++ Assets/Script/UI/SkillButton.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SkillButton : MonoBehaviour {
+  public Tooltip tooltip;
+  public Image image;
+  public Image cooldown;
+  public Button button;
+}
