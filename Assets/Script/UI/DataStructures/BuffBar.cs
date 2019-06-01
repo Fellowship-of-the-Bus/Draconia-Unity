d90dd91649881aec87dcfbef14d8c18e74dcb3ac
Abraham Dubrisingh <A.Dubrisingh@gmail.com>
2019-06-01 17:36:28 -0400

    Status bar improvements

--- Assets/Script/UI/DataStructures/BuffBar.cs
+++ Assets/Script/UI/DataStructures/BuffBar.cs
@@ -8,29 +8,30 @@ using System.Linq;
 public class BuffBar : MonoBehaviour {
 
   public GameObject button;
-  public Transform content;
 
   void Start() {
     get = this;
   }
 
   public void update(BattleCharacter selected) { //Call every time selection changes
-    foreach (Transform child in content) {
+    foreach (Transform child in transform) {
       if (child.gameObject) GameObject.Destroy(child.gameObject);
     }
     float offset = 0;
     foreach (Effect e in selected.getEffects()) {
-      GameObject b = GameObject.Instantiate(button, new Vector3 (0,0,0), Quaternion.identity, content) as GameObject;
-      b.GetComponentsInChildren<Text>()[0].text = e.GetType().Name;
+      GameObject b = GameObject.Instantiate(button, new Vector3 (0,0,0), Quaternion.identity, transform) as GameObject;
+
+      // TODO: Stop using the types. Support images that aren't for skills.
+      if (SkillList.get.skillImages[e.GetType()]) {
+        b.GetComponent<Image>().sprite = SkillList.get.skillImages[e.GetType()];
+      }
+
       offset += button.GetComponent<RectTransform>().rect.width;
       if (e is DurationEffect) {
         DurationEffect de = e as DurationEffect;
         b.AddComponent<Tooltip>();
         b.GetComponent<Tooltip>().tiptext = "Turns remaining: " + de.duration;
       }
-      //if (offset > bar.GetComponent<RectTransform>().rect.width) {
-      //  break;
-      //}
     }
   }
 
