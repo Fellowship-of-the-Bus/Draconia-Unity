daac1c2d3919d3864f6f4aa8a12c86f27c91b656
Rob Schluntz <rschlunt@uwaterloo.ca>
2019-06-01 18:04:11 -0400

    Simplify options menu; fixes #32

--- Assets/Script/Options.cs
+++ Assets/Script/Options.cs
@@ -1,97 +1,28 @@
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-[System.Serializable]
 public class Options {
-  [System.Serializable]
-  public abstract class OpField {
-    public GameObject panel;
-    public Text label;
-    protected string name;
-    public OpField(string name) {
-      this.name = name;
-    }
-    public abstract void init(GameObject o);
+  [Serializable]
+  public struct OptionData {
+    public float gridTransparency;
+    public bool displayAnimation;
   }
-  [System.Serializable]
-  public class OpToggleField : OpField {
-    public bool val;
-    public OpToggleField(bool val, string name) : base(name) {
-      this.val = val;
-      panel = (GameObject)Resources.Load("UI/Options/OpToggle");
-    }
-    public override void init(GameObject o) {
-      label = o.transform.Find("Label").gameObject.GetComponent<Text>();
-      label.text = name;
-      o.GetComponent<Toggle>().isOn = val;
-    }
-  }
-  [System.Serializable]
-  public class OpSliderField : OpField {
-    public float val;
-    public int min;
-    public int max;
-    public bool wholeValue;
-    public Text slideVal;
-    public Slider slider;
-    public OpSliderField(float val, int min, int max, string name, bool wholeValue = true) : base(name) {
-      this.val = val;
-      this.min = min;
-      this.max = max;
-      this.wholeValue = wholeValue;
-      panel = (GameObject)Resources.Load("UI/Options/OpSlider");
-    }
-    public override void init(GameObject o) {
-      label = o.transform.Find("Label").gameObject.GetComponent<Text>();
-      slideVal = o.transform.Find("Value").gameObject.GetComponent<Text>();
-      label.text = name;
-      slider = o.transform.Find("Slider").gameObject.GetComponent<Slider>();
-      slider.maxValue = max;
-      slider.minValue = min;
-      slider.wholeNumbers = wholeValue;
-      slider.value = val;
-      slideVal.text = val.ToString();
-    }
-  }
+  public static OptionData instance = new OptionData();
 
-  //To add a user visible field, add a static field to optionType
-  //add default value to displayedOptions
-  //add property for it
-  //note 'enum' value order must be same as order in array
-
-  //To add a user invisible field (for internal use only)
-  //make a static variable
-  [System.Serializable]
-  public class OptionType {
-    readonly int type;
-    public readonly static OptionType disAni = new OptionType(0);
-    public readonly static OptionType gridTransparency = new OptionType(1);
-
-    public OptionType(int i) {
-      type = i;
-    }
-
-    public static implicit operator int(OptionType x) {
-      return x.type;
-    }
-  }
-
-
-
-  public static OpField[] displayedOptions = new OpField[] {//Display Animation should default to true in real game
-    new OpToggleField(true, "Display Animations"),
-    new OpSliderField(1f,0,1, "Grid Transparency",false)
-  };
-  //Visible fields
   public static bool displayAnimation {
-    get { return ((OpToggleField)displayedOptions[OptionType.disAni]).val;}
-  }
-  public static float gridTransparency {
-    get { return ((OpSliderField)displayedOptions[OptionType.gridTransparency]).val;}
+    get { return instance.displayAnimation; }
+    set { instance.displayAnimation = value; }
   }
 
-  public class TextOptions {
+  public static float gridTransparency {
+    get { return instance.gridTransparency; }
+    set { instance.gridTransparency = value; }
+  }
+
+  [Serializable]
+  public struct TextOptions {
     public Font font;
     public delegate void FontChangedEvent(TMP_FontAsset newFont);
     public delegate void FontSizeChangedEvent(int newSize);
@@ -100,7 +31,7 @@ public class Options {
   }
   public static TextOptions Text = new TextOptions();
 
-  //internal use fields
+  // internal use fields - not serialized
   public static int FPS = 60;
   public static bool debugMode = true;
 }
--- Assets/Script/UI/SceneController/OptionController.cs
+++ Assets/Script/UI/SceneController/OptionController.cs
@@ -4,41 +4,17 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class OptionController: MonoBehaviour {
-  public Transform parent;
-
   void Start() {
-    int i = 0;
-    foreach (Options.OpField field in Options.displayedOptions) {
-      GameObject newPanel = Instantiate(field.panel,parent);
-      field.init(newPanel);
-      if (field is Options.OpToggleField) {
-        Toggle t = newPanel.GetComponent<Toggle>();
-        int j = i;
-        t.onValueChanged.AddListener((x) => {
-          onToggleChange(x, j);
-        });
-      } else if (field is Options.OpSliderField) {
-        Slider s = ((Options.OpSliderField)field).slider;
-        int j = i;
-        s.onValueChanged.AddListener((x) => {
-          onSliderChange(x, j);
-        });
-      }
-      i++;
-    }
+    gridTransparencyText.text = Options.gridTransparency.ToString();
   }
 
-  public void onToggleChange(bool val, int location) {
-    ((Options.OpToggleField)Options.displayedOptions[location]).val = val;
+  public void displayAnimations(bool newValue) {
+    Options.displayAnimation = newValue;
   }
 
-  public void onSliderChange(float val, int location) {
-    Options.OpSliderField op = (Options.OpSliderField)Options.displayedOptions[location];
-    op.val = val;
-    if (op.wholeValue) {
-      op.slideVal.text = ((int)val).ToString();
-    } else {
-      op.slideVal.text = val.ToString();
-    }
+  public Text gridTransparencyText;
+  public void gridTransparencyChanged(float newValue) {
+    Options.gridTransparency = newValue;
+    gridTransparencyText.text = newValue.ToString();
   }
 }
