5dbb062c1f4a30f95f3fd2f2c9489a3a600007e9
Rob Schluntz <rschlunt@uwaterloo.ca>
2020-01-01 11:29:13 -0500

    Add {Weapon/Item/Armour}Data scriptable object types and SerializableGuid for uniquely identifying assets in game for #100

---
+++ Assets/Script/Equipment/ArmourData.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ArmourData", menuName = "Items/ArmourData", order = 101)]
+public class ArmourData : ItemData {
+  public Armour.EquipmentClass equipmentClass;
+}
